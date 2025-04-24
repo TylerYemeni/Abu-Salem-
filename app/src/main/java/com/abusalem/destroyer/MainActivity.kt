9d8273f1abc55e3ebb829986b98a6fc992f820fb
TylerYemeni <leomessi111909@gmail.com>
2025-04-24 03:01:24 +0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/abusalem/destroyer/MainActivity.kt
@@ -0,0 +1,14 @@
+// MainActivity.kt
+package com.abusalem.destroyer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // هنا توضع الأكواد الخاصة بتشغيل التطبيق عند بدء التشغيل
+    }
+}
