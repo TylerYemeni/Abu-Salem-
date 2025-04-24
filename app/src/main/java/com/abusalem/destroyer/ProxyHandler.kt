ad6e331d823fc19033f4681561f63c0e36bebbe9
TylerYemeni <leomessi111909@gmail.com>
2025-04-24 03:11:53 +0300

    Create ProxyHandler.kt

---
+++ app/src/main/java/com/abusalem/destroyer/ProxyHandler.kt
@@ -0,0 +1,9 @@
+// ProxyHandler.kt
+package com.abusalem.destroyer
+
+object ProxyHandler {
+    fun changeIp() {
+        // هنا يتم تغيير الـ IP أو استخدام البروكسي
+        println("تم تغيير عنوان الـ IP.")
+    }
+}
